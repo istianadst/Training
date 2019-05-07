4579cfaca891ab1d1de6bfeeae71c4c480e68d19
Istiana Adesty <48156307+istianadst@users.noreply.github.com>
2019-05-07 14:29:58 +0700

    Create Strong Password.cpp

---
+++ Strong Password.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the minimumNumber function below.
+
+// Return the minimum number of characters to make the password strong
+
+int CharFamily(char c) 
+{
+   if(c >= '0' && c <= '9') return 0;
+   if(c >= 'a' && c <= 'z') return 1;
+   if(c >= 'A' && c <= 'Z') return 2;
+   return 3;
+}
+
+int main() 
+{
+    int n; cin >> n;
+    char c;
+    bitset<4> mask;
+
+    while (cin >> c)
+    {
+        mask.set(CharFamily(c));
+    }
+    cout << max(6-n, 4-(int)mask.count());
+
+
+    return 0;
+}
